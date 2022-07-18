5608512568096c05cbab8efe2c600b378ce9245f
lgcsantos <lucas.prog@outlook.com>
2022-07-18 20:56:38 -0300

    Primeiro commit

---
+++ src/test/java/com/lgcsantos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcsantos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
