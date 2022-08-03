1b91c83c1cea7813acd0ac25e224e2e89b114131
lgcsantos <lucas.prog@outlook.com>
2022-08-02 22:25:51 -0300

    Consulta simples com query methods

--- src/main/java/com/lgcsantos/workshopmongo/repository/PostRepositoy.java
+++ src/main/java/com/lgcsantos/workshopmongo/repository/PostRepositoy.java
@@ -1,5 +1,7 @@
 package com.lgcsantos.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.lgcsantos.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepositoy extends MongoRepository<Post, String> {
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);	
 
 	
 }
